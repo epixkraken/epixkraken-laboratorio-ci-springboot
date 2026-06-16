a94bfa9fc674f4172156375a00d7dd8e8b43de57
Epix Kraken <domenica.teran.iturralde@udla.edu.ec>
2026-06-16 18:11:19 -0500

    Create MyappApplication.java

---
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -0,0 +1,12 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappApplication {
+	public static void main(String[] modulation) {
+		SpringApplication.run(MyappApplication.class, modulation);
+		System.out.println("¡Aplicación Spring Boot ejecutada con éxito!");
+	}
+}
