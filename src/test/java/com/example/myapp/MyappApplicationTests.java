0da0cf9645e55ebb667ae2dfbb7a6b6ffe18bcd0
Epix Kraken <domenica.teran.iturralde@udla.edu.ec>
2026-06-16 18:11:59 -0500

    Create MyappApplicationTests.java

---
+++ src/test/java/com/example/myapp/MyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test básico de integración/unidad que verifica que la app levante bien
+	}
+}
